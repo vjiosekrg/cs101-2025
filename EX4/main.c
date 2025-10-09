c3d78ec526ace7c6b4d5befb72484965684dd860
vjiosekrg <231622123+vjiosekrg@users.noreply.github.com>
2025-10-09 15:42:55 +0800

    week6-1

--- EX4/main.c
+++ EX4/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void write(void* ary, size_t size, size_t count, FILE* fp) {
+    if (fwrite(ary, size, count, fp), ferror(fp) != 0) {
+        perror("Write file");
+        exit(1);
+    }
+}
+
+void read(void* ary, size_t size, size_t count, FILE* fp) {
+    if (fread(ary, size, count, fp), ferror(fp) != 0) {
+        perror("Read File");
+        exit(1);
+    }
+}
+
 int main() {
     int a[] = {0,1,2};
     char b[] = "ABC";
@@ -11,9 +25,9 @@ int main() {
         perror("open file");
         exit(1);
     }
-    fwrite(a, sizeof a[0], 3, fp);
-    fwrite(b, sizeof b[0], 4, fp);
-    fwrite(c, sizeof c[0], 3, fp);
+    write(a, sizeof a[0], 3, fp);
+    write(b, sizeof b[0], 4, fp);
+    write(c, sizeof c[0], 3, fp);
     fclose(fp);
 
     int ra[3] = {0};
@@ -25,9 +39,9 @@ int main() {
         perror("open file");
         exit(1);
     }
-    fread(ra, sizeof ra[0], 3, fp2);
-    fread(rb, sizeof rb[0], 4, fp2);
-    fread(rc, sizeof rc[0], 3, fp2);
+    read(ra, sizeof ra[0], 3, fp2);
+    read(rb, sizeof rb[0], 4, fp2);
+    read(rc, sizeof rc[0], 3, fp2);
 
     printf("%d %d %d\n", ra[0], ra[1], ra[2]);
     printf("%c %c %c\n", rb[0], rb[1], rb[2]);
